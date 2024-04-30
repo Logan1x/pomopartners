d07f36ca49966c8e8ac25aa88d41a20a12c6f113
logan1x <sharmakhushal78@gmail.com>
2024-04-30 21:10:21 +0530

    wip: supa auth

--- app/page.tsx
+++ app/page.tsx
@@ -7,44 +7,73 @@ import { Button } from "@/components/ui/button";
 
 import { supabase } from "@/config/supabaseConfig";
 
-const socket = io("http://localhost:3000");
-
 const App: React.FC = () => {
   const [timers, setTimers] = useState<{ [key: string]: any }>({});
+  const [uData, setUData] = useState<any>(null);
 
-  useEffect(() => {
-    // // Request the current timers when the component mounts
-    // socket.emit("request timers");
-    // // Update state when a timer update is received
-    // socket.on("timer update", (data) => {
-    //   setTimers(data);
-    // });
-    // // Clean up the event listener when the component unmounts
-    // return () => {
-    //   socket.off("timer update");
-    // };
+  // useEffect(() => {
+  //   // // Request the current timers when the component mounts
+  //   // socket.emit("request timers");
+  //   // // Update state when a timer update is received
+  //   // socket.on("timer update", (data) => {
+  //   //   setTimers(data);
+  //   // });
+  //   // // Clean up the event listener when the component unmounts
+  //   // return () => {
+  //   //   socket.off("timer update");
+  //   // };
+  //   // async function fetchData() {
+  //   //   try {
+  //   //     const { data, error } = await supabase.from("user").select();
+  //   //     console.log("error", error);
+  //   //   } catch (error) {
+  //   //     console.error("error", error);
+  //   //   }
+  //   // }
+  //   // fetchData();
+  // }, []);
 
-    async function fetchData() {
-      try {
-        const { data, error } = await supabase.from("user").select();
-
-        console.log("data", data);
-        console.log("error", error);
-      } catch (error) {
-        console.error("error", error);
-      }
+  const handleAnonymousLogin = async () => {
+    try {
+      const { data, error } = await supabase.auth.signInAnonymously();
+      console.log("error", error);
+    } catch (error) {
+      console.error("error", error);
     }
-    fetchData();
-  }, []);
+  };
+
+  const handleGithubLogin = async () => {
+    try {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "github",
+      });
+
+      console.log("data", data);
+      console.log("error", error);
+    } catch (error) {
+      console.error("error", error);
+    }
+  };
+
+  async function signOut() {
+    const { error } = await supabase.auth.signOut();
+    setUData(null);
+  }
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex gap-4 m-2">
       {/* <TimerDisplay timers={timers} />
       <PomodoroTimer /> */}
-      <Button variant={"default"} size={"sm"}>
-        Click me
+
+      <Button variant={"default"} size={"sm"} onClick={handleAnonymousLogin}>
+        Login Anonymously
       </Button>
-      Hello World
+      <Button variant={"default"} size={"sm"} onClick={handleGithubLogin}>
+        Login with Github
+      </Button>
+      <Button variant={"default"} size={"sm"} onClick={signOut}>
+        Sign Out
+      </Button>
     </div>
   );
 };
